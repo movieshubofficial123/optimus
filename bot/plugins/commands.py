4bb1280e13968d149b7c1517329832e26bf6ef71
movieshubofficial123 <85605801+movieshubofficial123@users.noreply.github.com>
2021-08-27 17:32:26 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -130,11 +130,11 @@ async def start(bot, update):
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await bot.send_message(
+   await bot.send_photo(
         chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(
+        photo = 'https://telegra.ph/file/5762017caebe0eb47fdd9.jpg',
+        caption=Translation.START_TEXT.format(
                 update.from_user.first_name),
-        reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=update.message_id
     )
